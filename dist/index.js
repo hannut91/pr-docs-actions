a096b60ebb9e78180f58081c5f581d8866244872
hannut91 <hannut91@gmail.com>
2022-04-21 22:02:19 +0900

    Build scripts

--- dist/index.js
+++ dist/index.js
@@ -8471,18 +8471,38 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(4247);
 const github = __nccwpck_require__(8432);
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+(async () => {
+  const myToken = core.getInput('myToken');
+
+  const owner = github.context.payload.repository.owner.login;
+  const repo = github.context.payload.repository.name;
+  const issueNumber = github.context.payload.number;
+
+  console.log('owner', owner);
+  console.log('repo', repo);
+  console.log('issueNumber', issueNumber);
+
+  const octokit = github.getOctokit(myToken);
+  const result = await octokit.rest.issues.listEventsForTimeline({
+    owner,
+    repo,
+    issue_number: issueNumber,
+  });
+  console.log('timeline', result);
+})();
+
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
 
 })();
 
