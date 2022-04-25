d121b89effe41827cb4625a7325339479bbae819
davidyang2149 <davidyang2149@gmail.com>
2022-04-25 22:48:17 +0900

    Add comment

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const { execSync } = require('child_process');
 
 const { createTempFolder } = require('./create-temp-folder');
 
+// comment1
 (async () => {
   const folder = await createTempFolder();
   execSync()
