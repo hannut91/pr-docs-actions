08c1fdadc1a4b13c906622ad55b5bd95c5fff497
David Yang <40143056+DavidYang2149@users.noreply.github.com>
2022-04-25 22:21:09 +0900

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-
-const { createTempFolder } = require('./create-temp-folder');
-
-(async () => {
-  const folder = await createTempFolder();
-  execSync()
-})();
