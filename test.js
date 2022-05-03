a3af1b524c110c7a3fc5988103330df01736ddf0
davidyang2149 <davidyang2149@gmail.com>
2022-05-03 23:25:30 +0900

    주석 메시지를 명확하게 만듭니다

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 
 const { createTempFolder } = require('./create-temp-folder');
 
-// comment2
+// for test message
 (async () => {
   const folder = await createTempFolder();
   execSync()
