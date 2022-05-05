612d06ccf31d8d50eaab641662841c7c15884027
hannut91 <hannut91@gmail.com>
2022-05-05 21:55:43 +0900

    Extract reading metadata

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 const { writeFile } = require('fs/promises');
-const { readFileSync } = require('fs');
 const { execSync } = require('child_process');
 
-const core = require('@actions/core');
 const github = require('@actions/github');
 
+const { readProperty } = require('./metadata');
 const { createTempFolder } = require('./create-temp-folder');
 
-const issueNumber = Number(readFileSync('./pr'));
-
+// const {
+//   githubToken,
+//   personalToken,
+//   owner,
+//   repo,
+//   issueNumber,
+// } = readProperty();
+// const timelines = readTimelines({
+//   githubToken,
+//   owner,
+//   repo,
+//   issueNumber,
+// });
+// const content = createContent(timelines);
+// uploadContent(content);
 (async () => {
-  const myToken = core.getInput('myToken');
-  const personalToken = core.getInput('personalToken');
+  // readProperty
+  const {
+    githubToken,
+    personalToken,
+    owner,
+    repo,
+    issueNumber,
+  } = readProperty();
 
-  const owner = github.context.payload.repository.owner.login;
-  const repo = github.context.payload.repository.name;
-  // const issueNumber = github.context.payload.number;
-
-  const octokit = github.getOctokit(myToken);
+  // readTimelines
+  const octokit = github.getOctokit(githubToken);
   const { data } = await octokit.rest.issues.listEventsForTimeline({
     owner,
     repo,
     issue_number: issueNumber,
   });
+
+  // createContent
   const content = data.reduce((acc, { message, body }) => {
     if (message) {
       return acc + `풀 리퀘스트 메시지: ${message}  \n`;
@@ -34,6 +51,7 @@ const issueNumber = Number(readFileSync('./pr'));
     return acc;
   }, '');
 
+  // uploadContent
   const folder = await createTempFolder();
 
   const wikiRepo = `https://${personalToken}@github.com/${process.env.GITHUB_REPOSITORY}.wiki.git`;
--- test.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-const { createTempFolder } = require('./create-temp-folder');
-
-// for test message
-(async () => {
-  const folder = await createTempFolder();
-  execSync()
-})();
